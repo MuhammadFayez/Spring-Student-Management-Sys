24aeff97c329c7d06c276b5bffdb39722b3977d1
Muhammad Fayez <it.dev37@dawateislami.net>
2023-02-14 16:00:53 +0500

    updated code

---
+++ student-management-system/src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -0,0 +1,21 @@
+package net.javaguides.sms;
+
+import net.javaguides.sms.entity.Student;
+import net.javaguides.sms.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+}
